1ef3d0a305a1ac43a5305f62d5fb78e1efb8ca76
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2019-01-04 12:27:04 -0600

    Add lifetime example
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

---
+++ book/10-generics/generic-lifetimes/src/main.rs
@@ -0,0 +1,17 @@
+// Lifetime annotations allow the borow checker to reject any parameters that
+// don't adhere to the specified lifetime constraints.
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
